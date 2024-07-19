881c48a56550082f8f706abb655b68af5a0572d1
Vishal Gautam <ivishalgautamm@gmail.com>
2024-07-19 13:53:32 +0530

    add metadata in some pages

--- web-fe/src/app/products/page.jsx
+++ web-fe/src/app/products/page.jsx
@@ -1,15 +1,13 @@
 import Categories from "../categories";
 
 export const metadata = {
-  title:
-    "MS Rails, GI Binding Wire, MS Binding Wire, Round Pipe, CR Sheet, HPRO Sheets, PPGl Coil, GI Coil",
+  title: "Products | Infrakeys",
   description:
     "Infrakeys offer quality Product MS Rails, GI Binding Wire, MS Binding Wire, Round Pipe, PPGl Coil, GI Coil, Ledger 2mm, Vertical 3mm, PPGL Coil 0.5mm at the best prices",
   keywords:
     "MS Rails Suppliers, GI Binding Wire Manufacturers, MS Binding Wire, GI Binding Wire Suppliers near me, Round PipeSuppliers, CR Sheet suppliers, HPRO Sheets Suppliers, PPGl Coil Manufacturers, GI Coil Manufacturers, MS Rails Suppliers, GI Binding Wire Suppliers, MS Binding Wire Suppliers, Round Pipe Suppliers, CR Sheet Suppliers, HPRO Sheets Suppliers, PPGl Coil Suppliers, GI Coil Suppliers",
   openGraph: {
-    title:
-      "MS Rails, GI Binding Wire, MS Binding Wire, Round Pipe, CR Sheet, HPRO Sheets, PPGl Coil, GI Coil",
+    title: "Products | Infrakeys",
     description:
       "Infrakeys offer quality Product MS Rails, GI Binding Wire, MS Binding Wire, Round Pipe, PPGl Coil, GI Coil, Ledger 2mm, Vertical 3mm, PPGL Coil 0.5mm at the best prices",
   },
